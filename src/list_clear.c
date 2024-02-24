bdef92291712c467805a7758f70f9aac1616a3fc
DURAND Malo <malo.durand@epitech.eu>
2024-02-24 20:40:35 +0100

    tracking the list_clear.c file

---
+++ src/list_clear.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "list.h"
+
+void list_clear(list_t *list)
+{
+    list_elem_t *next = NULL;
+
+    if (NULL == list)
+        return;
+    while (NULL != list->elems_head) {
+        next = list->elems_head->next;
+        if (NULL != list->destroy)
+            list->destroy(list->elems_head->elem);
+        free(list->elems_head);
+        list->elems_head = next;
+    }
+    list->elems_head = NULL;
+    list->elems_tail = NULL;
+    list->count = 0;
+}
