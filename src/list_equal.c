e3f19c718a41aca2c3b9a99632a2f526ff66e204
DURAND Malo <malo.durand@epitech.eu>
2024-02-26 11:07:37 +0100

    adding the list_equal function to compare two lists depending on either the content of elements or their address.

---
+++ src/list_equal.c
@@ -0,0 +1,35 @@
+#include "list.h"
+
+static int list_equal_address(list_elem_t const *head1,
+    list_elem_t const *head2)
+{
+    while (NULL != head1 && NULL != head2) {
+        if (head1->elem != head2->elem)
+            return 0;
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    if ((NULL == head1 && NULL != head2) || (NULL != head1 && NULL == head2))
+        return 0;
+    return 1;
+}
+
+int list_equal(list_t const *list1, list_t const *list2, list_equal_cmp_t cmp)
+{
+    list_elem_t const *head1 = list1->elems_head;
+    list_elem_t const *head2 = list2->elems_head;
+
+    if (list1 == list2)
+        return 1;
+    if (NULL == cmp)
+        return list_equal_address(head1, head2);
+    while (NULL != head1 && NULL != head2) {
+        if (0 != cmp(head1->elem, head2->elem))
+            return 0;
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    if ((NULL == head1 && NULL != head2) || (NULL != head1 && NULL == head2))
+        return 0;
+    return 1;
+}
---
+++ tests/tests_list_equal.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <criterion/criterion.h>
+#include <criterion/new/assert.h>
+#include "list.h"
+
+static int my_strcmp(void const *s1, void const *s2)
+{
+    return strcmp((char const *) s1, (char const *) s2);
+}
+
+Test(list_equal, test_impl)
+{
+    list_t *list1 = list_new(free);
+    list_t *list2 = list_new(free);
+
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my head !"), 0)));
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my tail !"), 1)));
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my tail 2-1 !"), -1)));
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my tail 2 !"), 2)));
+    cr_assert(eq(int, 0, list_equal(list1, list2, NULL)));
+    cr_assert(eq(int, 1, list_equal(list1, list1, NULL)));
+    cr_assert(eq(int, 1, list_equal(list2, list2, NULL)));
+    cr_assert(eq(int, 0, list_equal(list1, list2, my_strcmp)));
+    cr_assert(eq(int, 0, list_insert_at(list2, strdup("This is my head !"), 0)));
+    cr_assert(eq(int, 0, list_insert_at(list2, strdup("This is my tail !"), 1)));
+    cr_assert(eq(int, 0, list_insert_at(list2, strdup("This is my tail 2-1 !"), -1)));
+    cr_assert(eq(int, 0, list_insert_at(list2, strdup("This is my tail 2 !"), 2)));
+    cr_assert(eq(int, 0, list_equal(list1, list2, NULL)));
+    cr_assert(eq(int, 1, list_equal(list1, list1, NULL)));
+    cr_assert(eq(int, 1, list_equal(list2, list2, NULL)));
+    cr_assert(eq(int, 1, list_equal(list1, list2, my_strcmp)));
+    free(list_remove_at(list2, -1));
+    cr_assert(eq(int, 0, list_push_back(list2, strdup("This is my tail 3 !"))));
+    cr_assert(eq(int, 0, list_equal(list1, list2, my_strcmp)));
+    list_destroy(list1);
+    list_destroy(list2);
+}
+
+Test(list_equal, test_impl_address_cmp)
+{
+    list_t *list1 = list_new(free);
+    list_t *list2 = list_new(NULL);
+
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my head !"), 0)));
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my tail !"), 1)));
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my tail 2-1 !"), -1)));
+    cr_assert(eq(int, 0, list_insert_at(list1, strdup("This is my tail 2 !"), 2)));
+    cr_assert(eq(int, 0, list_push_back(list2, list_value_at(list1, 0))));
+    cr_assert(eq(int, 0, list_push_back(list2, list_value_at(list1, 1))));
+    cr_assert(eq(int, 0, list_push_back(list2, list_value_at(list1, 2))));
+    cr_assert(eq(int, 0, list_push_back(list2, list_value_at(list1, 3))));
+    cr_assert(eq(int, 1, list_equal(list1, list2, NULL)));
+    cr_assert(eq(int, 1, list_equal(list1, list2, my_strcmp)));
+    list_destroy(list1);
+    list_destroy(list2);
+}
